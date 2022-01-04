2246b78b8d0d75c6d4b001e6533f434d67ad074a
up201806490 <up201806490@fe.up.pt>
2022-01-04 10:17:50 +0000

    attempt

--- src/get.py
+++ src/get.py
@@ -17,7 +17,7 @@ log.setLevel(logging.DEBUG)
 
 async def run():
     server = Server()
-    await server.listen(8001)
+    await server.listen(interface="127.0.0.1",port=8000)
     bootstrap_node = (sys.argv[1], int(sys.argv[2]))
     await server.bootstrap([bootstrap_node])
 
--- src/node.py
+++ src/node.py
@@ -20,6 +20,7 @@ def parse_arguments():
     # Optional arguments
     parser.add_argument("-i", "--ip", help="IP address of existing node", type=str, default=None)
     parser.add_argument("-p", "--port", help="port number of existing node", type=int, default=None)
+    parser.add_argument("-b", "--bootstrap", help="Bootstrap on or off", type=bool, default=False)
 
     return parser.parse_args()
 
@@ -28,7 +29,7 @@ def connect_to_bootstrap_node(args):
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
 
-    loop.run_until_complete(server.listen(8469))
+    loop.run_until_complete(server.listen(interface=args.ip, port=args.port))
     bootstrap_node = (args.ip, int(args.port))
     loop.run_until_complete(server.bootstrap([bootstrap_node]))
 
@@ -41,11 +42,12 @@ def connect_to_bootstrap_node(args):
         loop.close()
 
 
-def create_bootstrap_node():
+def create_bootstrap_node(args):
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
 
-    loop.run_until_complete(server.listen(8468))
+    
+    loop.run_until_complete(server.listen(interface=args.ip, port=args.port))
 
     try:
         loop.run_forever()
@@ -59,7 +61,7 @@ def create_bootstrap_node():
 def create_node(ip,port):
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
-    loop.run_until_complete(server.listen(port))
+    loop.run_until_complete(server.listen(interface="127.0.0.1", port=port))
 
     bootstrap_node = (ip, port)
     loop.run_until_complete(server.bootstrap([bootstrap_node]))
@@ -73,14 +75,18 @@ def create_node(ip,port):
         loop.close()
 
 
-
 def main():
     args = parse_arguments()
 
-    create_node(args.ip,args.port)
+    if(args.bootstrap):
+        create_bootstrap_node(args)
+    else:
+        connect_to_bootstrap_node(args)
 
-    # bootstrap_node = (ip, porta)
-    # loop.run_until_complete(server.bootstrap([bootstrap_node]))
+
+    # create_node(args.ip,args.port)
+
+
 
     # if args.ip and args.port:
     #     connect_to_bootstrap_node(args)
